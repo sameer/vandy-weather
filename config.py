00361ca32f48c4fcf3d963205f011cabbf5657dd
Sameer Puri <sameer@users.noreply.github.com>
2019-10-28 09:10:50 -0500

    Don't use half float for cuda...causing CUDNN_STATUS_BAD_PARAM

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ WINDOW_SIZE: int = 15
 
 if torch.cuda.is_available():
     DEVICE = torch.device('cuda')
-    DTYPE = torch.float16
+    DTYPE = torch.float32
 else:
     DEVICE = torch.device('cpu')
     DTYPE = torch.float32
